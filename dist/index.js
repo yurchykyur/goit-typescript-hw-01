bdeb7372bfe3ad9cf5bcbb9f73ac53b85529f4c3
yurchykyur <yurchyk.yur.s@gmail.com>
2023-10-15 15:14:58 +0300

    finished commit

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,13 @@
-"use strict";
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+// add(1, 1);
+import { concatenation } from "./concatenation";
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+if (button && input) {
+    button.addEventListener("click", () => {
+        concatenation(input.value, "hello!");
+    });
+}
 //# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// function add(num1: number, num2: number) {
+//   return num1 + num2;
+// }
+
+// add(1, 1);
+
+import { concatenation } from "./concatenation";
+
+const button = document.querySelector("button")!;
+const input = document.querySelector("input")!;
+
+if (button && input) {
+  button.addEventListener("click", () => {
+    concatenation(input.value, "hello!");
+  });
+}
